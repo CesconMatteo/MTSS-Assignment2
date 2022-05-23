06415dbb0f60cf58a80e5e958c438290efea6c47
Cescon Matteo <cesconmatteo18@gmail.com>
2022-05-23 11:23:05 +0200

    Aggiunto sconto processori close #2

--- src/test/java/it/unipd/mtss/BillTest.java
+++ src/test/java/it/unipd/mtss/BillTest.java
@@ -35,4 +35,55 @@ public class BillTest {
         o.getOrderPrice(itemList, new UserImpl(1, "s", "n", "f", " f", LocalDate.of(2000, 3, 3)));
         fail();
     }
+    
+    @Test
+    public void CheckProcessor_ZeroProcessors () {
+        EItem e  = new EItemImpl("1", 10, ItemType.Processor);
+        List<EItem> itemList = new ArrayList<EItem>(Collections.nCopies(3, e));
+        double sum = BillImpl.checkProcessor(30.0,0,itemList);
+        assertEquals(30, sum, 0.0);
+    }
+    
+    @Test
+    public void CheckProcessor_LessThenZeroProcessor () {
+        EItem e  = new EItemImpl("1", 10, ItemType.Processor);
+        List<EItem> itemList = new ArrayList<EItem>(Collections.nCopies(3, e));
+        double sum = BillImpl.checkProcessor(30.0,-4,itemList);
+        assertEquals(30, sum, 0.0);
+    }
+
+    @Test
+    public void CheckProcessor_LessThenFiveProcessor () {
+        EItem e  = new EItemImpl("1", 10, ItemType.Processor);
+        List<EItem> itemList = new ArrayList<EItem>(Collections.nCopies(3, e));
+        double sum = BillImpl.checkProcessor(30.0,2,itemList);
+        assertEquals(30, sum, 0.0);
+    }
+
+    @Test
+    public void CheckProcessor_MoreThenFiveProcessor () {
+        int counter = 10;
+        EItem e  = new EItemImpl("1", 10, ItemType.Processor);
+        List<EItem> itemList = new ArrayList<EItem>(Collections.nCopies(counter, e));
+        double sum = BillImpl.checkProcessor(100.0, counter,itemList);
+        assertEquals(95, sum, 0.0);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void CheckProcessor_NegativeSum () {
+        int counter = 10;
+        EItem e  = new EItemImpl("1", 10, ItemType.Processor);
+        List<EItem> itemList = new ArrayList<EItem>(Collections.nCopies(counter, e));
+        double sum = BillImpl.checkProcessor(-45.0, counter,itemList);
+        assertEquals(95, sum, 0.0);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void CheckProcessor_SumEqualToZero () {
+        int counter = 10;
+        EItem e  = new EItemImpl("1", 10, ItemType.Processor);
+        List<EItem> itemList = new ArrayList<EItem>(Collections.nCopies(counter, e));
+        double sum = BillImpl.checkProcessor(0.0, counter,itemList);
+        assertEquals(95, sum, 0.0);
+    }
 }
